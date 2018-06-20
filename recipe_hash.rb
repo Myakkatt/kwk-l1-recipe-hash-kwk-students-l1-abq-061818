c15962dbb173c4b931b0fddfff1a0c58e18a3c54
Myakkatt <myao_03@icloud.com>
2018-06-20 21:15:07 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,14 +1,14 @@
 recipe_ingredients = {
-almond flour => 2 cups
-gluten_freewhole_oats => 1 cup
-kosher_salt => 1/2 tea spoon
-baking_powder => 1/2 tea spoon
-baking_soda => 1/4 tea spoon
-xanthan_gum => 1/2 tea spoon
-slivered_almonds lightly_toasted => 1/4 cup
- mini_dark_chocolate_chips => 3/4 cup
-olive_oil => 1/2 cup
- agave => 1/4 cup
+"almond flour" => 2 cups
+"gluten_freewhole_oats" => 1 cup
+"kosher_salt" => 1/2 tea spoon
+"baking_powder" => 1/2 tea spoon
+"baking_soda" => 1/4 tea spoon
+"xanthan_gum "=> 1/2 tea spoon
+"slivered_almonds lightly_toasted" => 1/4 cup
+ "mini_dark_chocolate_chips" => 3/4 cup
+"olive_oil" => 1/2 cup
+ "agave" => 1/4 cup
 }
 
 
